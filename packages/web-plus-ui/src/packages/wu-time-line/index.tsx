da876faaee6134bbddcf98c7f2049a656ac4734f
Marvin <yongfeide123@gmail.com>
2022-05-25 01:35:05 +0800

    feat:add timeline component

---
+++ packages/web-plus-ui/src/packages/wu-time-line/index.tsx
@@ -0,0 +1,29 @@
+﻿import { h, Component, Prop, Provide } from '@canyuegongzi/web-core-plus';
+import css from './index.scss';
+
+@Component({
+    name: 'wu-plus-timeline',
+    css: css,
+})
+export class WuTimeLine extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    @Prop({ default: false, type: Boolean })
+    public reverse: boolean;
+
+    @Provide("timelineRef")
+    public timelineRef() {
+        return this;
+    }
+
+    public render(_renderProps = {}, _store = {}) {
+        const reverse = this.reverse;
+        return (
+            <ul class={`wu-timeline ${reverse? reverse: 'is-reverse'}`}>
+                <slot />
+            </ul>
+        );
+    }
+}
