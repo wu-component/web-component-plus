53c35f70d1096b4e258aceb6cbed1d80623dcb56
Marvin <yongfeide123@gmail.com>
2022-05-29 23:01:22 +0800

    feat:add container component

--- packages/web-plus-ui/src/packages/wu-container/index.tsx
+++ packages/web-plus-ui/src/packages/wu-container/index.tsx
@@ -1,1 +1,47 @@
-export class WuContainer {}
+﻿import { h, Component, Prop, OnConnected, OnBeforeUpdate } from '@canyuegongzi/web-core-plus';
+import css from './index.scss';
+
+type DirectionEnums = 'vertical' | 'horizontal' | any;
+@Component({
+    name: 'wu-plus-container',
+    css: css,
+})
+export class WuContainer extends HTMLElement implements OnConnected, OnBeforeUpdate {
+    constructor() {
+        super();
+    }
+
+    public isVertical = false;
+
+    public beforeUpdate() {
+        this.isVertical = this.initIsVertical();
+    }
+
+    public connected(shadowRoot: ShadowRoot) {
+        this.isVertical = this.initIsVertical();
+        (this as any).update();
+    }
+
+    private initIsVertical() {
+        const slotDom: any = this.shadowRoot.getElementById('defaultSlot');
+        const list: any[] = slotDom?.assignedNodes();
+        if (!list || !list.length) {
+            return false;
+        }
+        return slotDom.assignedNodes().some(item => {
+            return item.tagName === 'WU-PLUS-HEADER' || item.tagName === 'WU-PLUS-FOOTER';
+        });
+    }
+
+    @Prop({ default: '', type: String })
+    public direction: DirectionEnums;
+
+
+    public render(_renderProps = {}, _store = {}) {
+        return (
+            <section class={`${this.isVertical? 'wu-container is-vertical': 'wu-container'}`}>
+                <slot id="defaultSlot" class={`${this.isVertical? 'wu-container is-vertical': 'wu-container'}`}  />
+            </section>
+        );
+    }
+}
